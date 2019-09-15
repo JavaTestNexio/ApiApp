ae58f775e166e0f7e8c0e2d7d58c80174a6ae544
Tarek OUAAZ <tarek.ouaaz@dz.gt.com>
2019-09-15 01:17:53 +0100

    Mise à jour

--- src/main/java/com/test/ApiApp/repository/ProduitRepository.java
+++
@@ -1,9 +0,0 @@
-package com.test.ApiApp.repository;
-
-import com.test.ApiApp.domain.Produit;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProduitRepository extends CrudRepository<Produit, Integer> {
-
-}
