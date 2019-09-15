d284fa84a586efd43e990408e908551587b0f72f
JavaTestNexio <55333768+JavaTestNexio@users.noreply.github.com>
2019-09-16 01:06:53 +0200

    Add files via upload

--- src/test/java/com/test/ApiApp/ApiAppApplicationTests.java
+++ src/test/java/com/test/ApiApp/ApiAppApplicationTests.java
@@ -1,8 +1,10 @@
 package com.test.ApiApp;
 
+
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
 
 
 @RunWith(SpringRunner.class)
